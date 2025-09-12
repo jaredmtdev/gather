bb262bb253c8b440e11fc4b71818142f6b907315
jaredmt <211057370+jaredmtdev@users.noreply.github.com>
2025-09-12 15:22:36 -0400

    set up a pipeline example. added readme. cleaned up.

---
+++ examples/pipeline/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"time"
+	"together/examples/internal/samplegen"
+	"together/examples/internal/samplemiddleware"
+	"together/pkg/together"
+)
+
+func main() {
+	mw := samplemiddleware.Logger[int, int]("INFO", "ERROR")
+	addHandler := func(num int) together.Handler[int, int] {
+		return func(ctx context.Context, in int, scope *together.Scope[int]) (int, error) {
+			select {
+			case <-time.After(time.Duration(rand.Intn(200)) * time.Millisecond):
+			case <-ctx.Done():
+				return 0, ctx.Err()
+			}
+			return num + in, nil
+		}
+	}
+	wrappedAddHandler := func(num int) together.Handler[int, int] {
+		return mw(addHandler(num))
+	}
+	ctx := context.Background()
+
+	// example of a pipeline
+	out1 := together.Workers(ctx, samplegen.Range(40), wrappedAddHandler(1), together.WithWorkerSize(5))
+	out2 := together.Workers(ctx, out1, wrappedAddHandler(2), together.WithWorkerSize(3))
+	for range together.Workers(ctx, out2, wrappedAddHandler(3), together.WithWorkerSize(2), together.WithBufferSize(2)) {
+	}
+
+	// should not see anymore noise from the pipeline at this point
+	fmt.Println("done!")
+	time.Sleep(time.Second)
+	fmt.Println("shutting down")
+}
--- pkg/together/stage.go
+++ pkg/together/stage.go
@@ -6,35 +6,6 @@ import (
 	"time"
 )
 
-/*
-the goal is to offload as much of the complexities as possible to middleware
-keep Workers as simple/intuitive as possible
-This way the worker can focus more on business logic
-
-keep everything in one package for now. 3 main components:
--Workers
--Middleware
--Pipeline
-
-
-what if the user wants an error channel + return type? what if they want a single return type that includes both?
--return type is always provided. for separate error channel: user controls all generators. they can use their "work" func to send err to errCh
-
-what if user wants to stop on first error like errgroup?
-they can cancel context on first error and handle error which ever way they want (send to err channel or handle right here)
-
-what if they want to collect all errors and results?
-user can send errors to err channel. a separate worker can consume them
-
-what if user wants retries? (probably want to track number of retries for each data)
--use WorkHandler for this (used within "work")
-
-
-***what if we want to do something after worker shuts down? (like w.Close())
-***what if we want to execute something periodically? (like w.Flush())
-
-*/
-
 // Scope - gives the user some ability to do things that require internal mechanisms
 type Scope[IN any] struct {
 	enqueue   func(v IN)
@@ -57,13 +28,13 @@ func (s *Scope[IN]) RetryAfter(ctx context.Context, in IN, after time.Duration) 
 	})
 }
 
+// Retry - will retry immediately
 func (s *Scope[IN]) Retry(ctx context.Context, in IN) {
 	s.RetryAfter(ctx, in, 0)
 }
 
 // allow your work to safely spin up a new go routine (in addition to the worker go routine)
-//
-// this might be useful for patterns like ReplyTo
+// the worker will stay alive until this go routine completes
 func (s *Scope[IN]) Go(f func()) {
 	s.wgJob.Go(f)
 }
@@ -102,7 +73,7 @@ func newWorkerStation() *workerStation {
 	}
 }
 
-// Workers - build concurrency model based on the handler and options
+// Workers - build a single pipeline stage based on the handler and options
 // error handling is done within work func. in there the user can:
 func Workers[IN any, OUT any](ctx context.Context, in <-chan IN, handler Handler[IN, OUT], opts ...Opt) <-chan OUT {
 	ws := newWorkerStation()
