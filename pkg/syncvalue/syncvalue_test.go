17f88ad14660f61cf7a89161b9e673a879002537
jaredmt <211057370+jaredmtdev@users.noreply.github.com>
2025-09-15 20:33:58 -0400

    set up tests for syncvalue

---
+++ pkg/syncvalue/syncvalue_test.go
@@ -0,0 +1,36 @@
+package syncvalue_test
+
+import (
+	"sync"
+	"testing"
+	"together/pkg/syncvalue"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConcurrentLoad(t *testing.T) {
+	v := syncvalue.Value[int]{}
+	v.Store(3)
+
+	wg := sync.WaitGroup{}
+	for range 1000 {
+		wg.Go(func() {
+			assert.Equal(t, 3, v.Load())
+		})
+	}
+	wg.Wait()
+}
+
+func TestConcurrentStore(t *testing.T) {
+	v := syncvalue.Value[int]{}
+	wg := sync.WaitGroup{}
+	for i := range 1000 {
+		wg.Go(func() {
+			v.Store(i)
+		})
+	}
+	wg.Wait()
+
+	assert.LessOrEqual(t, 0, v.Load())
+	assert.Less(t, v.Load(), 1000)
+}
