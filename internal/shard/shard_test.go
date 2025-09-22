6e7f633ddfffdb7220fb83f43731776ff08f45ae
jaredmt <211057370+jaredmtdev@users.noreply.github.com>
2025-09-22 13:47:48 -0400

    add more tests. handle nil input channels

--- internal/shard/shard_test.go
+++ internal/shard/shard_test.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"gather"
 	"gather/internal/shard"
+	"gather/internal/syncvalue"
+	"sync"
+	"sync/atomic"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -32,4 +35,76 @@ func TestApply(t *testing.T) {
 		}
 	}
 	assert.Equal(t, 5, total)
+}
+
+func TestApplyWithNoBuffer(t *testing.T) {
+	ctx := context.Background()
+	ins := make([]<-chan int, 5)
+	wgGen := sync.WaitGroup{}
+	for i := range ins {
+		wgGen.Add(1)
+		go func() {
+			inCh := make(chan int)
+			ins[i] = inCh
+			wgGen.Done() // just need to make sure all channels are set before sending to shards
+			inCh <- 1
+			close(inCh)
+		}()
+	}
+	handler := func() gather.HandlerFunc[int, int] {
+		return func(_ context.Context, in int, _ *gather.Scope[int]) (int, error) {
+			return in, nil
+		}
+	}
+	wgGen.Wait()
+	outs := shard.Apply(ctx, ins, handler(), gather.WithBufferSize(0))
+	assert.Len(t, outs, 5)
+	var total atomic.Int32
+	wg := sync.WaitGroup{}
+	for _, out := range outs {
+		wg.Go(func() {
+			for v := range out {
+				assert.Equal(t, 1, v)
+				total.Add(1)
+			}
+		})
+	}
+	wg.Wait()
+	assert.Equal(t, int32(5), total.Load())
+}
+
+func TestApplyWithMultipleWorkersAndNoBuffer(t *testing.T) {
+	ctx := context.Background()
+	ins := make([]<-chan int, 5)
+	wgGen := sync.WaitGroup{}
+	for i := range ins {
+		wgGen.Add(1)
+		go func() {
+			inCh := make(chan int)
+			ins[i] = inCh
+			wgGen.Done() // just need to make sure all channels are set before sending to shards
+			inCh <- 1
+			close(inCh)
+		}()
+	}
+	handler := func() gather.HandlerFunc[int, int] {
+		return func(_ context.Context, in int, _ *gather.Scope[int]) (int, error) {
+			return in, nil
+		}
+	}
+	wgGen.Wait()
+	outs := shard.Apply(ctx, ins, handler(), gather.WithBufferSize(0), gather.WithWorkerSize(2))
+	assert.Len(t, outs, 5)
+	var total syncvalue.Value[int]
+	wg := sync.WaitGroup{}
+	for _, out := range outs {
+		wg.Go(func() {
+			for v := range out {
+				assert.Equal(t, 1, v)
+				total.Store(total.Load() + v)
+			}
+		})
+	}
+	wg.Wait()
+	assert.Equal(t, 5, total.Load())
 }
