68be2fe0172369b9e91330e0b719992478623b59
jaredmt <211057370+jaredmtdev@users.noreply.github.com>
2025-09-21 22:41:02 -0400

    added some notes. experimented with an example for sharding

--- internal/shard/doc.go
+++ internal/shard/doc.go
@@ -1,6 +1,10 @@
 // shard - package used to multiply stages into shards
-// in most cases: just use together.Workers
-// when you have a lot of jobs that complete instantly, sharding can help improve performance by breaking up into multiple channels
+// in most cases: just use gather.Workers
+//
+// some situations where sharding might make sense:
+// - when you have a lot of jobs that complete instantly, sharding can help improve performance by breaking up into multiple channels
+// - you want to split the work by keys (each key gets it's own ordering or rate limiting etc)
+// - when a stage multiplies data - sharding can help contain an occasional large data multiplication to a single key (and not block progress on other keys)
 //
 // this package is currently experimental
 package shard
