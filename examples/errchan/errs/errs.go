0463ba76c7b2e5ef93573a884086ba00d8aa1f98
jaredmtdev <211057370+jaredmtdev@users.noreply.github.com>
2025-09-29 17:00:41 -0400

    fixed small errors. Need to fix cognitive complexity next

--- examples/errchan/errs/errs.go
+++ examples/errchan/errs/errs.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 )
 
+// ErrInvalidNumber - error when invalid number is detected.
 var ErrInvalidNumber = errors.New("invalid number")
 
+// NewErrInvalidNumber - creates a new ErrInvalidNumber.
 func NewErrInvalidNumber(in int) error {
 	return fmt.Errorf("%v: %w", in, ErrInvalidNumber)
 }
