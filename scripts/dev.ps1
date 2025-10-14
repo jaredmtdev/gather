4cc1ec7cdc2d2967577ba167604795c2f67f5654
jaredmtdev <211057370+jaredmtdev@users.noreply.github.com>
2025-10-14 08:43:59 -0400

    tested powershell. made some improvements to both dev scripts

--- scripts/dev.ps1
+++ scripts/dev.ps1
@@ -6,7 +6,7 @@ $ErrorActionPreference = "Stop"
 $HostPath = (Get-Location).Path
 
 docker run -it --rm `
-  -v "$HostPath:/gather" `
+  -v "${HostPath}:/gather" `
   -w /gather `
   golang:1.25.1@sha256:d7098379b7da665ab25b99795465ec320b1ca9d4addb9f77409c4827dc904211 `
-  bash
+  bash -c 'go mod tidy && bash'
